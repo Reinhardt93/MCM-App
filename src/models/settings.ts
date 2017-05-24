e2b35c9d237fd3c7156876d3ace99b768e80a84a
jsundwall <johan.sundwall@gmail.com>
2017-05-24 18:18:29 +0200

    small settings fix

--- src/models/settings.ts
+++ src/models/settings.ts
@@ -3,15 +3,11 @@ export class Settings
     constructor()
     {
     }
-    angler_name: boolean;
-    datetime: boolean;
-    fishing_method: boolean;
-    breed: boolean;
-    length: boolean;
-    weight: boolean;
-    weather: boolean;
-    location: boolean;
-    latitude: boolean;
-    longitude: boolean;
+
+    notification: boolean;
+    newsletter: boolean;
+    something: boolean;
+    fourth: boolean;
+
 
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -16,56 +16,25 @@ export class SettingsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage)
   {
     this.settings = new Settings();
-    this.storage.get('angler_name').then((val) => {
-      this.settings.angler_name = val;
-    });
-    this.storage.get('datetime').then((val) => {
-      this.settings.datetime = val;
-    });
-    this.storage.get('breed').then((val) => {
-      this.settings.breed = val;
-    });
-    this.storage.get('length').then((val) => {
-      this.settings.length = val;
-    });
-    this.storage.get('weight').then((val) => {
-      this.settings.weight = val;
-    });
-    this.storage.get('weather').then((val) => {
-      this.settings.weather = val;
-    });
-    this.storage.get('location').then((val) => {
-      this.settings.location = val;
-    });
-    this.storage.get('latitude').then((val) => {
-      this.settings.latitude = val;
-    });
-    this.storage.get('longitude').then((val) => {
-      this.settings.longitude = val;
-    });
-
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+    this.getData()
+  }
+
+  getData() {
+    this.storage.get('notification').then((val) => {
+      this.settings.notification = val;
+    });
+    this.storage.get('newsletter').then((val) => {
+      this.settings.newsletter = val
+    });
   }
 
   setData(){
-    console.log(this.settings.angler_name);
-    this.storage.set('angler_name',this.settings.angler_name);
-    this.storage.set('datetime',this.settings.datetime);
-    this.storage.set('breed',this.settings.breed);
-    this.storage.set('length',this.settings.length);
-    this.storage.set('weight',this.settings.weight);
-    this.storage.set('weather',this.settings.weather);
-    this.storage.set('location',this.settings.location);
-    this.storage.set('latitude',this.settings.latitude);
-    this.storage.set('longitude',this.settings.longitude);
-    console.log(this.settings.angler_name)
-
-    // this.navCtrl.push(HomePage, {
-    //     angler_name: this.settings.angler_name
-    // });
+    this.storage.set('notification',this.settings.notification);
+    this.storage.set('newsletter',this.settings.newsletter);
   }
 
 }
